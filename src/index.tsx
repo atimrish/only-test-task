e011db8a0f1a1c48e435ec070e6c50c92bfe8d8b
atimrish <strcty11@gmail.com>
2025-06-17 11:09:00 +0400

    [add] конфигурация webpack

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import {createRoot} from "react-dom/client";
+import {App} from "@src/app/App";
+import './index.scss'
+
+const rootElement = document.getElementById("root") as HTMLDivElement;
+const root = createRoot(rootElement);
+root.render(<App />);
---
+++ webpack.config.ts
@@ -0,0 +1,72 @@
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import DevServer from "webpack-dev-server";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+
+interface WebpackEnv {
+	mode?: webpack.Configuration["mode"];
+	port?: number;
+}
+
+export default (env: WebpackEnv): webpack.Configuration => {
+	const isProduction = env.mode === "production";
+
+	return {
+		mode: env.mode || "development",
+		entry: "./src/index.tsx",
+		devtool: "inline-source-map",
+		module: {
+			rules: [
+				{
+					test: /\.s[ac]ss$/,
+					use: [
+						MiniCssExtractPlugin.loader,
+						{
+							loader: "css-loader",
+							options: {
+								importLoaders: 1,
+								modules: true,
+							},
+						},
+						"sass-loader",
+					],
+				},
+				{
+					test: /\.tsx?$/,
+					use: "ts-loader",
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.(png|svg|jpg|jpeg|gif)$/i,
+					type: "asset/resource",
+				},
+			],
+		},
+		resolve: {
+			extensions: [".ts", ".tsx", ".js"],
+			alias: {
+				"@src": path.resolve("src"),
+			},
+		},
+		output: {
+			filename: "bundle-[contenthash:8].js",
+			path: path.resolve("dist"),
+			clean: true,
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: path.resolve("src", "index.html"),
+				minify: isProduction,
+			}),
+			new MiniCssExtractPlugin({filename: "style-[contenthash:8].css"}),
+		],
+		optimization: {
+			minimizer: [new CssMinimizerPlugin()],
+		},
+		devServer: {
+			port: env.port || 8000,
+		},
+	};
+};
